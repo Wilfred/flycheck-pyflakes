c26ce4d0462f42f450382c8cd2f03b9ba86e988a
Wilfred Hughes <me@wilfred.me.uk>
2014-04-29 00:25:40 +0100

    Documenting usage.

--- flycheck-pyflakes.el
+++ flycheck-pyflakes.el
@@ -8,7 +8,17 @@
 
 ;;; Commentary:
 
-;; TODO
+;; This package adds support for pyflakes to flycheck. To use it, add
+;; to your init.el:
+
+;; (require 'flycheck-pyflakes)
+
+;; If you want to use pyflakes you probably don't want pylint or
+;; flake8. To disable those checkers, add the following to your
+;; init.el:
+
+;; (add-to-list 'flycheck-disabled-checkers 'python-flake8)
+;; (add-to-list 'flycheck-disabled-checkers 'python-pylint)
 
 ;;; License:
 
